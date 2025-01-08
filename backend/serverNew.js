3f683e136a2f3c714677944f1c81e26cd4830b4f
terenceyin <terence.yin76@gmail.com>
2025-01-08 01:14:46 -0800

    got rid of trailing slash

--- backend/serverNew.js
+++ backend/serverNew.js
@@ -11,7 +11,7 @@ const dbKey = process.env.DB_KEY || process.env.dbKey;
 
 
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'https://scriber-126cc.web.app/'); 
+  res.header('Access-Control-Allow-Origin', 'https://scriber-126cc.web.app'); 
   res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS'); 
   res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization'); 
   next();
