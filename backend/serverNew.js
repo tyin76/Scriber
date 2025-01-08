b95c99b2d348a140b3c4d11f63b3b1ca7d448d1e
terenceyin <terence.yin76@gmail.com>
2025-01-08 01:00:05 -0800

    more cors

--- backend/serverNew.js
+++ backend/serverNew.js
@@ -9,6 +9,22 @@ const appController = require('../backend/controller/controller');
 
 const dbKey = process.env.DB_KEY || process.env.dbKey;
 
+
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*'); 
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS'); 
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization'); 
+  next();
+});
+
+
+app.options('*', (req, res) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  res.sendStatus(200); 
+});
+
 const corsOptions = {
   origin: (origin, callback) => {
     const allowedOrigins = [
@@ -26,24 +42,6 @@ const corsOptions = {
   },
 };
 
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*'); 
-  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS'); 
-  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization'); 
-  next();
-});
-
-
-app.options('*', (req, res) => {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-  res.sendStatus(200); 
-});
-
-
-app.use(cors(corsOptions));
-app.options('*', cors(corsOptions));
 app.use(express.json());
 
 
